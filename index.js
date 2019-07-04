9e834b928689d320e8158ba4f45601abb93b29e6
Marc Queiroz <marc.queiroz@gmail.com>
2019-07-04 15:58:20 -0300

    Fixing web3js libraries.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-
-Web3 = require('web3');
-
-if (typeof web3 != 'undefined')
-{
-	web3 = new Web3(web3.currentProvider);
-	console.log("existing web3: provider " + typeof web3);
-	console.log(web3.currentProvider);
+function init() {
+  
+  if (typeof web3 != 'undefined')
+  {
+  	web3 = new Web3(web3.currentProvider);
+  	console.log("existing web3: provider " + typeof web3);
+  	console.log(web3.currentProvider);
+  }
+  else{
+  	web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/"));
+  	console.log("new provider " + web3);
+  }
+  
+  console.log(web3.isConnected());
+  console.log(web3.eth.accounts[0]);
+  
+  var ProfContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"fname","type":"string"},{"name":"lname","type":"string"},{"name":"id","type":"uint256"}],"name":"setProfessor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getProfessor","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"firstname","type":"string"},{"indexed":false,"name":"lastname","type":"string"},{"indexed":false,"name":"collegeid","type":"uint256"}],"name":"ProfessorEv","type":"event"}]);
+  
+  contractInstance = ProfContract.at('0x79813b97613730932989905869566f2b2d28b1fe');
+  console.log (contractInstance);
+  
+  var fname = "John";
+  var lname = "Doe";
+  var id = 731;
 }
-else{
-	web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/"));
-	console.log("new provider " + web3);
-}
-
-console.log(web3.isConnected());
-console.log(web3.eth.accounts[0]);
-
-var ProfContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"fname","type":"string"},{"name":"lname","type":"string"},{"name":"id","type":"uint256"}],"name":"setProfessor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getProfessor","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"firstname","type":"string"},{"indexed":false,"name":"lastname","type":"string"},{"indexed":false,"name":"collegeid","type":"uint256"}],"name":"ProfessorEv","type":"event"}]);
-
-contractInstance = ProfContract.at('0x79813b97613730932989905869566f2b2d28b1fe');
-console.log (contractInstance);
-
-var fname = "John";
-var lname = "Doe";
-var id = 731;
 
 function getProfessor() {
   contractInstance.getProfessor((error, result) => {
